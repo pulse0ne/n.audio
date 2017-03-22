be6561d3bccddcf3c16fc5e6722afe1e4c31bebd
Tyler Snedigar <snedigart@gmail.com>
2017-03-21 23:41:24 -0500

    First commit

---
+++ naudio.js
@@ -0,0 +1,16 @@
+/**
+ * Created by tsned on 3/21/17.
+ */
+'use strict';
+
+// var MPlayer = require('mplayer');
+// var player = new MPlayer();
+//
+// player.on('start', console.log.bind(this, 'playback started'));
+// player.on('time', console.log);
+//
+// player.openPlaylist('http://www.miastomuzyki.pl/n/rmfclassic.pls', {cache: 128, cacheMin: 1});
+//
+// setTimeout(player.volume.bind(player, 50), 10000);
+
+
---
+++ public/app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by tsned on 3/21/17.
+ */
+'use strict';
+
+var naudio = angular.module('n.audio', ['ngMaterial']);
+
+naudio.controller('n.audio.controller.main', [
+    '$scope',
+    '$rootScope',
+    '$'
+    function () {
+
+    }
+]);
